5c0dd2cb67e18d3f795be045e9f3dcc8940e0495
Franjo Lovrić <franjolovric23@gmail.com>
2025-03-06 09:03:50 +0100

    Styled root layout and navigation, displaying logo and username

--- src/layouts/RootLayout/RootLayout.jsx
+++ src/layouts/RootLayout/RootLayout.jsx
@@ -5,10 +5,11 @@ const RootLayout = () => {
     return (
         <div className="RootLayout">
             <header>
-                <Link to="/">
+                <Link to="/" className="Logo">
                     <img src="/logo.png" alt="logo" />
                     <span>CROTECHBOT</span>
                 </Link>
+                <div className="User">User</div>
             </header>
             <main>
                 <Outlet />

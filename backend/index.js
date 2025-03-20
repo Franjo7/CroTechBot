a19eb6ccefbfacb4978ef25a7e4a9dd10fa76f78
Franjo Lovrić <franjolovric23@gmail.com>
2025-03-20 09:03:17 +0100

    Connected to MongoDB Atlas using Mongoose

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import ImageKit from 'imagekit';
+import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,6 +11,16 @@ dotenv.config();
 app.use(cors({
     origin: process.env.CORS_ORIGIN
 }));
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB');
+    }
+    catch (error) {
+        console.error('Error connecting to MongoDB');
+    }
+}
 
 const imagekit = new ImageKit({
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
@@ -23,5 +34,6 @@ app.get('/api/upload', (req, res) => {
 });
 
 app.listen(port, () => {
+    connect();
     console.log(`Server is running on port ${port}`);
 });

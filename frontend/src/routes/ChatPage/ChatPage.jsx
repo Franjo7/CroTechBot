a5a08a87eb445ac8997ffc4e28fdf7d61c855e99
Franjo Lovrić <franjolovric23@gmail.com>
2025-03-28 09:05:10 +0100

    Added functionality to fetch a single chat

--- frontend/src/routes/ChatPage/ChatPage.jsx
+++ frontend/src/routes/ChatPage/ChatPage.jsx
@@ -1,13 +1,46 @@
+import React from 'react';
 import './ChatPage.css';
 import NewPrompt from '../../components/NewPrompt/NewPrompt';
+import { useQuery } from '@tanstack/react-query';
+import { useLocation } from 'react-router-dom';
+import Markdown from 'react-markdown';
+import { IKImage } from 'imagekitio-react';
 
 const ChatPage = () => {
+
+    const path = useLocation().pathname;
+    const chatId = path.split("/").pop();
+
+    const { isPending, error, data } = useQuery({
+        queryKey: ["chat", chatId],
+        queryFn: () => 
+            fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+                credentials: 'include'
+            }).then(res => res.json())
+    });
+
     return (
         <div className="ChatPage">
             <div className="wrapper">
                 <div className="chat">
-                    <div className="message">Hello! I am a chatbot called CroTechBot. How can I help you today?</div> 
-                    <div className="message user">Can you help me with my coding assignment? I am stuck on a problem.</div>
+                    {isPending ? <div>Loading...</div> : error ? <div>Something went wrong...</div> : data?.history?.map((message, index) => (
+                        <React.Fragment key={index}>
+                            {message.image && (
+                                <IKImage 
+                                    urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT}
+                                    path={message.image}
+                                    height={300}
+                                    width={400}
+                                    transformation={[{ height: 300, width: 400 }]}
+                                    loading='lazy'
+                                    lqip={{ active: true, quality: 20 }}
+                                />
+                            )}
+                            <div className={message.role === "user" ? "message user" : "message"}>
+                                <Markdown>{message.parts[0].text}</Markdown>
+                            </div>
+                        </React.Fragment>
+                    ))}
                     <NewPrompt />
                 </div>
             </div>
